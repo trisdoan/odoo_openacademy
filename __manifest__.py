21f38f63fb5134e6837fc3576adb7553b2baf217
tris <doanminhtri8183@gmail.com>
2023-02-09 15:54:51 +0700

    stop at basic view

--- __manifest__.py
+++ __manifest__.py
@@ -17,8 +17,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/openacademy_views.xml',
+        'views/openacademy_menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
